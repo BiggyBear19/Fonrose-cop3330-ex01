3677a0401a609922e6131edb733fc6d0a22e8dc2
BiggyBear19 <bernardthebear19@gmail.com>
2021-09-13 19:11:15 -0400

    updated

--- src/main/java/Exercise1/Exercise1.java
+++ src/main/java/Exercise1/Exercise1.java
@@ -1,2 +1,20 @@
-package Exercise1;public class Exercise1 {
+package Exercise1;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Bernard Fonrose
+ */
+
+import java.util.Scanner;
+
+public class Exercise1 {
+    public static void main(String[] args) {
+        Scanner name = new Scanner(System.in);
+
+        System.out.print("What is your Name? ");
+
+        String Name = name.nextLine();
+
+        System.out.printf("\nHello, %s, nice to meet you!", Name);
+    }
 }
